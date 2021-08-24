d5ed51b2b3642bbcc18947229a05b099b9e2eee2
vidya <vidyas098new@gmail.com>
2021-08-24 16:27:17 +0530

    added basic authentication

--- src/main/java/com/sample/demo/RecipeProjectApplication.java
+++ src/main/java/com/sample/demo/RecipeProjectApplication.java
@@ -3,6 +3,7 @@ package com.sample.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -17,6 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @SpringBootApplication
 @EnableSwagger2
+@EnableWebSecurity
 public class RecipeProjectApplication {
 
 	/**
